38bfb7b3a7118b2a80ec1dd7c62d3f9a0d12e4bc
Damien Seguin <dmnsgn@gmail.com>
2015-05-28 17:38:18 +0100

    Add vendor script

---
+++ scripts/utils.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var _fs = require('fs');
+
+function concatFiles(opts) {
+  var out = opts.src.map(function(filePath) {
+    return _fs.readFileSync(filePath).toString();
+  });
+  _fs.writeFileSync(opts.dest, out.join('\n'));
+}
+
+module.exports = {
+  concatFiles: concatFiles
+};
