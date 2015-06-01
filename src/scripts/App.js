530f54d93ee78b71d69fdc4988ac4f8753fe5bc6
Damien Seguin <dmnsgn@gmail.com>
2015-06-01 10:57:42 +0100

    Add UI and sketch

--- src/scripts/App.js
+++ src/scripts/App.js
@@ -2,6 +2,14 @@ import AppView from './view/AppView'
 
 export default class App {
   constructor() {
-    this.view = new AppView();
+
+    this.DEBUG = true;
+
+    this.initView();
+  }
+
+  initView() {
+    this.view = new AppView(this, document.getElementById('container'));
+    this.view.init();
   }
 };
--- src/scripts/view/AppView.js
+++ src/scripts/view/AppView.js
@@ -1,7 +1,59 @@
 import AppUI from './AppUI'
 
 export default class AppView {
-  constructor() {
-    this.ui = new AppUI();
+  constructor(app, el) {
+    this.app = app;
+    this.el = el;
   }
+
+  init() {
+    this.initAnimationLoop();
+
+    this.ctx.start();
+
+    if (this.app.DEBUG) {
+      this.initUI();
+    }
+
+    return;
+  }
+
+  initAnimationLoop() {
+    this.ctx = Sketch.create({
+      type      : Sketch.WEB_GL,
+      container : this.el,
+      autostart : false,
+      autopause : true
+    });
+
+    this.ctx.setup = () => {
+    };
+
+    this.ctx.update = () => {
+      if (this.app.DEBUG) {
+        this.ui.stats.begin();
+      }
+
+
+      if (this.app.DEBUG) {
+        this.ui.stats.end();
+      }
+    };
+
+    this.ctx.draw = () => {
+
+    };
+    this.ctx.resize = () => {
+
+    };
+    return;
+  }
+
+  initUI() {
+
+    this.ui = new AppUI();
+
+    return;
+  }
+
 };
