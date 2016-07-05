0bf9291a077a0efdba7cc7810ff2c3f625aa0e5d
Damien Seguin <dmnsgn@gmail.com>
2016-07-05 17:03:57 +0100

    Use control kit instead of dat

--- src/util/window.js
+++ src/util/window.js
@@ -1,18 +1,23 @@
 const w = {
   init() {
-    this.onResize(this.updateDimensions.bind(this));
-    this.updateDimensions();
+    this.onResize(this.update.bind(this));
+    this.update();
   },
 
   width: null,
   height: null,
+  center: null,
   devicePixelRatio: window.devicePixelRatio || 1,
 
-  updateDimensions() {
+  update() {
     this.width =
       window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
     this.height =
       window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+    this.center = {
+      x: this.width / 2,
+      y: this.height / 2
+    };
   },
 
   onResize(listener, options = false) {
